24fa4e659232a669341075faf36b1b9e349a2168
Danil Pismenny <danil@brandymint.ru>
2020-01-06 22:08:06 +0300

    Fix telegram in test env

--- config/initializers/telegram.rb
+++ config/initializers/telegram.rb
@@ -2,6 +2,8 @@
 
 # Copyright (c) 2019 Danil Pismenny <danil@brandymint.ru>
 
+Telegram.bots_config = { default: '12312321:adasdadasa' } if Rails.env.test?
+
 Rails.application.config.telegram_updates_controller.session_store = :file_store, './tmp/session_store'
 
 Rails.application.config.telegram_updates_controller.logger =
